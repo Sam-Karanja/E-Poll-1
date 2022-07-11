9693b2f7cd3bf7b2131e0d4f764fffb57880f8c2
FloWambui <gflorencewambui@gmail.com>
2022-07-11 15:07:57 +0300

    add the accounts and epoll app models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,90 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
 
+
+# class CustomUser(AbstractUser):
+#     email = models.CharField(max_length=255, unique=True)
+#     password = models.CharField(max_length=255)
+#     username = models.CharField(max_length=255, blank=True, null=True)
+#     is_voter = models.BooleanField(default=False)
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ["username"]
+
+
+from unicodedata import name
+# import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.hashers import make_password
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+# customUsermanager, customModel
+
+
+class UserManager(BaseUserManager):
+	def create_user(self, email, password=None):
+		"""
+		Create and return a `User` with an email, username and password.
+		"""
+		if not email:
+			raise ValueError('Users Must Have an email address')
+		user = self.model(
+			email=self.normalize_email(email),
+		)
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
+
+	def create_superuser(self, email, password):
+		"""
+		Create and return a `User` with superuser (admin) permissions.
+		"""
+		if password is None:
+			raise TypeError('Superusers must have a password.')
+		user = self.create_user(email, password)
+		user.is_superuser = True
+		user.is_staff = True
+		user.save()
+		return user
+
+	def create_voteruser(self, email, password):
+		if password is None:
+			raise TypeError('Voter must have a password')
+		user = self.create_user(email, password)
+		user.is_voter = True
+		user.save()
+		return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(
+    	verbose_name='email address', max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_voter = models.BooleanField(default=True)
+    USERNAME_FIELD = 'email'    # username field is email
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- epoll/models.py
+++ epoll/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from accounts.models import * 
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class Voter(models.Model):
+    admin = models.ManyToManyField(CustomUser)
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=11, unique=True)  
+    email = models.EmailField(max_length=255, unique=True)
+    verified = models.BooleanField(default=False)
+    voted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    max_vote = models.IntegerField()
+    priority = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Candidate(models.Model):
+    fullname = models.CharField(max_length=50)
+    image = CloudinaryField('image')
+    bio = models.TextField()
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.fullname
+
+class Votes(models.Model):
+    voter = models.ForeignKey(Voter, on_delete=models.CASCADE)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
